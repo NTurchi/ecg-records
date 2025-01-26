ec63162dbc3f68f37b57770e46be175a507e3efc
NTurchi <nicolas.turchi@hotmail.fr>
2025-01-26 14:09:49 +0100

    feat: add query mock

---
+++ src/core/helpers/query-test-utils.ts
@@ -0,0 +1,22 @@
+import { WritableSignal, signal } from '@angular/core';
+import { CreateMutationResult, CreateQueryResult } from '@tanstack/angular-query-experimental';
+
+/**
+ * Return a tanstack query mock object
+ * In a huge app, we would create a proper mock with all types required in CreateQueryResult
+ * But for the sake of this example, we will just return a simple object
+ */
+export const getSuccessQueryMock = <T>(data: WritableSignal<T>) =>
+  ({
+    data: data,
+    isLoading: signal(false),
+    isSuccess: signal(true),
+    isError: signal(false),
+  }) as unknown as CreateQueryResult<T, Error>;
+
+export const getMutationQueryMock = <T, K>() =>
+  ({
+    isSuccess: signal(true),
+    mutate: (args: K) => {},
+    mutateAsync: (args: K) => Promise.resolve(),
+  }) as unknown as CreateMutationResult<T, Error, K>;
